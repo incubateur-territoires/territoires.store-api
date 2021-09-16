366a9cb7a7881befcb5500169286e4be9147c266
Jonathan Pichot <jonathan@pichot.us>
2021-09-16 15:49:07 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors')
+
+// var indexRouter = require('./routes/index');
+var storeRequestRouter = require('./routes/store-request');
+
+var app = express();
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// app.use('/', indexRouter);
+app.use('/store-request', storeRequestRouter);
+
+module.exports = app;
